254bfb0015b271527b76deb674d9e4ec4bb2714b
paperspace <byl006@ucsd.edu>
2023-06-14 21:57:47 +0000

    added basic starter scripts

---
+++ scripts/step2_get_sequence_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Get the assembly report. This contains summaries of all genomes on NCBI. You can use this find information for your species. In our case it will be for Lytechinus pictus.
+wget https://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt
